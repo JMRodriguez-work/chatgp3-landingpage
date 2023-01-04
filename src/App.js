1866742138175bcabf7d7621facb5433ea7ce8e7
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-01-04 18:53:19 -0300

    Agregando primeros archivos para comenzar el proyecto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TbAmbulance } from "react-icons/tb";
+
+const App = () => {
+  return (
+    <h1>
+        <TbAmbulance />
+    </h1>
+  )
+}
+
+export default App
